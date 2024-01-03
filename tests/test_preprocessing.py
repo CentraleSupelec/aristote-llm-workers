d86fa2e2258c2515a069838bfedec805ba7045e6
Antonio Loison <antonio.loison@illuin.tech>
2024-01-03 17:42:19 +0100

    fix: add transcript example

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -6,13 +6,14 @@ from quiz_generation.preprocessing.preprocessing import (
     get_token_nb,
     load_txt,
 )
+from quiz_generation.dtos.dtos import TranscribedText
 
 
 @pytest.mark.parametrize(
     "path, expected_texts",
     [
         [
-            "data/tests/bio_transcript.txt",
+            "tests/example/bio_transcript.txt",
             [
                 "[Music]",
                 "okay today we are on the last section of",
@@ -28,10 +29,14 @@ from quiz_generation.preprocessing.preprocessing import (
     ],
 )
 def test_load_txt(path, expected_texts):
-    texts = load_txt(path)
-    assert len(texts) == len(expected_texts)
+    transcripts = load_txt(path)
+    assert isinstance(transcripts[0], TranscribedText)
+    assert len(transcripts) == len(expected_texts)
     assert all(
-        [text == expected_text for text, expected_text in zip(texts, expected_texts)]
+        [
+            transcript.text == expected_text
+            for transcript, expected_text in zip(transcripts, expected_texts)
+        ]
     )
 
 
@@ -40,40 +45,70 @@ def test_load_txt(path, expected_texts):
     [
         [
             [
-                "the the the the the",
+                TranscribedText(
+                    text="the the the the the",
+                    start=0,
+                    end=1,
+                ),
             ],
             "mistralai/Mistral-7B-v0.1",
             2,
-            ["the", "the", "the", "the", "the"],
+            [
+                TranscribedText(
+                    text="the",
+                    start=0,
+                    end=0.2,
+                ),
+                TranscribedText(
+                    text="the",
+                    start=0.2,
+                    end=0.4,
+                ),
+                TranscribedText(
+                    text="the",
+                    start=0.4,
+                    end=0.6,
+                ),
+                TranscribedText(
+                    text="the",
+                    start=0.6,
+                    end=0.8,
+                ),
+                TranscribedText(
+                    text="the",
+                    start=0.8,
+                    end=1,
+                ),
+            ]
         ],
         [
             [
-                "[Music]",
-                "okay today we are on the last section of",
-                "chapter 9 section 9.3 we're still",
-                "talking about energy but today we're not",
-                "gonna talk about energy in plants we're",
-                "gonna talk about cellular respiration",
-                "we're gonna talk about how that process",
-                "works and I talked about photosynthesis",
-                "we're talking about cellular respiration",
+                TranscribedText(text="[Music]"),
+                TranscribedText(text="okay today we are on the last section of"),
+                TranscribedText(text="chapter 9 section 9.3 we're still"),
+                TranscribedText(text="talking about energy but today we're not"),
+                TranscribedText(text="gonna talk about energy in plants we're"),
+                TranscribedText(text="gonna talk about cellular respiration"),
+                TranscribedText(text="we're gonna talk about how that process"),
+                TranscribedText(text="works and I talked about photosynthesis"),
+                TranscribedText(text="we're talking about cellular respiration"),
             ],
             "mistralai/Mistral-7B-v0.1",
             30,
             [
-                (
+                TranscribedText(text=(
                     "[Music] okay today we are on the last section of chapter 9 "
                     "section 9.3 we're still"
-                ),
-                (
+                )),
+                TranscribedText(text=(
                     "talking about energy but today we're not gonna talk about energy"
                     " in plants we're gonna talk about cellular respiration"
-                ),
-                (
+                )),
+                TranscribedText(text=(
                     "we're gonna talk about how that process works and "
                     "I talked about photosynthesis"
-                ),
-                "we're talking about cellular respiration",
+                )),
+                TranscribedText(text="we're talking about cellular respiration"),
             ],
         ],
     ],
@@ -81,11 +116,18 @@ def test_load_txt(path, expected_texts):
 def test_get_splits(transcripts, tokenizer_name, max_length, expected_splits):
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
     splits = get_splits(transcripts, tokenizer, max_length)
+    assert isinstance(splits[0], TranscribedText)
     assert len(splits) == len(expected_splits)
-    assert all([get_token_nb(split, tokenizer) <= max_length for split in splits])
+    assert all([get_token_nb(split.text, tokenizer) <= max_length for split in splits])
     assert all(
         [
-            split == expected_split
+            split.text == expected_split.text
+            for split, expected_split in zip(splits, expected_splits)
+        ]
+    )
+    assert all(
+        [
+            split.start == expected_split.start and split.end == expected_split.end
             for split, expected_split in zip(splits, expected_splits)
         ]
     )
